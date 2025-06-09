71217d2ddd1a4fbd48cd011e85f727f73f50049c
Harsh Kumawat <harshkumawat324@gmail.com>
2025-06-09 11:43:44 +0530

    testing 2

--- MyFirstApp8Jan/MyFirstApp8Jan/ContentView.swift
+++ MyFirstApp8Jan/MyFirstApp8Jan/ContentView.swift
@@ -11,22 +11,33 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
             
-            Text("Helllo World! This is my first SwiftUI app!")
-                .font(.largeTitle)
-                .padding()
-            
-            Button(action: {
-                print("Button tapped!")
-            }) {
-                Text("Tap Me")
+            VStack {
+                Text("Helllo World! This is my first SwiftUI app!")
+                    .font(.largeTitle)
                     .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                
+                
+                Button(action: {
+                    print("Button tapped!")
+                }) {
+                    Text("Tap Me")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .position(x: geometry.size.width / 2, y: geometry.size.height / 2) // Center position
+                
+                Group {
+                    Text("Hello")
+                    Text("World")
+                    Text("Hello")
+                    Text("World")
+                }.bold()
+                
             }
-            .position(x: geometry.size.width / 2, y: geometry.size.height / 2) // Center position
         }
-        .frame(width: 400, height: 400)  // Parent view size
+        .frame(width: 400, height: 600)  // Parent view size
         .background(Color.red)
     }
     
